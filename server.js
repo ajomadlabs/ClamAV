3729beccf376f436f9273a212c00220bed2b41b4
ajomadlabs <ajojohn555@gmail.com>
2018-07-20 11:16:03 +0530

    create basic server file

---
+++ server.js
@@ -0,0 +1,25 @@
+// Requiring the Modules
+
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// Defining the app
+
+const app = express()
+
+// Defining the port
+
+const port = process.env.PORT || 9000
+
+// Defining the middlewares
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// Listening to port
+
+app.listen(port)
+console.log('Server Running on port:' + ' ' + 'http://localhost:' + port)
